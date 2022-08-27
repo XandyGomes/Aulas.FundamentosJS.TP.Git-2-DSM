71e823ac750316e2db3eb202e314560fb84fac00
XandyGomes <x3.computadores@hotmail.com>
2022-08-26 22:21:14 -0300

    Aula.26.08.2022

---
+++ VarLet.js
@@ -0,0 +1,17 @@
+// var numero = 1
+
+// { //bloco não é função
+//     var numero = 2 // imprimi esse último valor
+//     console.log('dentro do bloco =', numero)
+// }
+// console.log("fora do bloco =", numero)
+
+let num = 1
+
+{ //bloco não é função
+    let num = 2 // imprimi esse último valor
+    console.log('dentro do bloco =', num)
+}
+console.log("fora do bloco =", num)
+
+
---
+++ funcao2.js
@@ -0,0 +1,15 @@
+const imprimirSoma = function (a, b){
+    console.log(a+b)
+}
+imprimirSoma(2,3)
+
+const soma = (a,b) => {  //ARROW FUNCTION
+   return a + b
+}
+console.log(soma(5,7))
+
+const sub = (a,b) => a - b
+console.log(sub(10,2))
+
+const imp = a => console.log(a)
+imp("Olha o tanto que é prático!!!")
