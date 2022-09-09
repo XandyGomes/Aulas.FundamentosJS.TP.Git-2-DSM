c452342a2365f7bc70e1332e701f60ca7c7bc39a
XandyGomes <x3.computadores@hotmail.com>
2022-09-08 22:30:20 -0300

    Aula.08.09.2022

--- Exercicios/exerc01.js
+++ Exercicios/exerc01.js
@@ -1,4 +1,7 @@
 /*01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma,
 subtração, multiplicação e divisão desses valores.*/
 
-
+function calcularOperacoes(operador1, operador2){
+    console.log(`Soma = ${operador1 + operador2}, Subtração = ${operador1 - operador2}, Multiplicação = ${operador1 * operador2}, Divisão = ${operador1 / operador2}`)
+}
+calcularOperacoes(2,1)
